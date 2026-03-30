6d19b344ac11bf1536195c8a7908c64b8afffe1b
kapkap <167351369+mycapcap@users.noreply.github.com>
2026-03-30 23:25:06 +0000

    Create gamePhysics.js

---
+++ js/gamePhysics.js
@@ -0,0 +1,5 @@
+//our 3d garbage rolls and make the garbage under roll
+//the moutain of garbage is pure gravity, if it falls, it rolls, if it rolls, garbage under will roll but a bit slower
+class gravity {
+  
+}
